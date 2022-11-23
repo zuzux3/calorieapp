bb7bd988c6e8afef89344341d5d93f13011fe6c8
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-11-23 21:56:23 +0100

    problem to solve:
    RecursionError: maximum recursion depth exceeded while calling a Python object

--- calorie_app/calorie_app/settings.py
+++ calorie_app/calorie_app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'calorieTracker.apps.CaloriesConfig',
+    'calorieTracker.apps.CalorietrackerConfig',
     'django_filters'
 ]
 
