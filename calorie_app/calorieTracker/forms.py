1b8dd4648fab668203128228a3a01165206d9144
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-11-23 22:22:52 +0100

    Create forms.py

---
+++ calorie_app/calorieTracker/forms.py
@@ -0,0 +1,29 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import Food, Profile
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class SelectFoodForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('foodSelected', 'quantity',)
+
+        def __init__(self, user, *args, **kwargs):
+            super(SelectFoodForm, self).__init__(*args, **kwargs)
+            self.fields['foodSelected'].queryset = Food.objects.filter(personOf=user)
+
+class AddFoodForm(forms.ModelForm):
+    class Meta:
+        model = Food
+        fields = ('name', 'quantity', 'calorie',)
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('calorieGoal')
