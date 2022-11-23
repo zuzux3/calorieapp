52caed065735eb5ef089c613713153ed24d212fa
Zuzanna Tarazewicz <68703753+zuzux3@users.noreply.github.com>
2022-11-23 21:43:37 +0100

    models.py ended and admin.py code added

--- calorie_app/calorieTracker/models.py
+++ calorie_app/calorieTracker/models.py
@@ -15,4 +15,28 @@ class Food(models.Model):
 class Profile(models.Model):
     personOf = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
     calorieCount = models.FloatField(default=0,null=True,blank=True)
-    
+    foodSelected = models.ForeignKey(Food, on_delete=models.CASCADE,null=True,blank=True)
+    quantity = models.FloatField(default=0)
+    totalCalorie = models.FloatField(default=0,null=True)
+    date = models.DateField(auto_now_add=True)
+    calorieGoal = models.PositiveBigIntegerField(default=0)
+    allFoodSelectedToday = models.ManyToManyField(Food,through='PostFood',related_name='inventory')
+
+    def save(self, *args, **kwargs):
+        if self.foodSelected != None:
+            self.amount = (self.foodSelected.calorie/self.foodSelected.quantity)
+            self.calorieCount = self.amount*self.quantity
+            calories = Profile.objects.filter(personOf = self.personOf).last()
+            PostFood.objects.create(profile=calories,food=self.foodSelected,calorieAmount=self.calorieCount, amount=self.quantity)
+            self.foodSelected = None
+            super(Profile, self).save(*args, **kwargs)
+
+        else:
+            super(Profile, self).save(*args, **kwargs)
+
+
+class PostFood(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    calorieAmount = models.FloatField(default=0,null=True,blank=True)
+    amount = models.FloatField(default=0)
